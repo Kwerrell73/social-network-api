3b3a5b881df473cd7777df87e9b36f4f9d372838
Kellie Werrell <kelliewerrell@gmail.com>
2021-07-14 22:09:54 -0700

    added users and thoughts models

---
+++ models/Thoughts.js
@@ -0,0 +1,73 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment's _id field
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      trim: true
+
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    thoughtBody: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 299
+
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReactionSchema to validate data for a reply
+    reaction: [ReactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+ThoughtsSchema.virtual('replyCount').get(function() {
+  return this.reactions.length;
+});
+
+// create the Thoughts model using the Thoughts Schema
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Comment;
