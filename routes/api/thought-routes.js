4d98d7f4c2446b1c9872ac5264755ab221902f7e
Kellie Werrell <kelliewerrell@gmail.com>
2021-07-18 15:35:42 -0700

    added thought routes for get * getbyid * Put Delete and Post * added reaction routes for Post and Delete

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,20 +1,34 @@
 // Require express router
 const router = require('express').Router();
 
-// Set requirements 
-//const {
-//get all thoughts
-//get thoughts by id
-//create thoughts
-//update
-//delete thought
-//add reaction
-//delete reaction
+// Set requirements
 
-//} = require('../../controllers/thoughts-controller');
+const {
+getAllThoughts,
+getThoughtsById,
+createThoughts,
+updateThoughts,
+deleteThoughts,
+addReaction,
+deleteReaction
+
+} = require('../../controllers/thoughts-controller');
 
 
+// api/thoughts <GET>
+router.route('/').get(getAllThoughts);
 
+// api/thoughts/** id <GET, PUT, DELETE>
+router.route('/:id').get(getThoughtsById).put(updateThoughts).delete(deleteThoughts); 
+
+// api/thoughts/** userId <POST>
+router.route('/:userId').post(createThoughts);
+
+// api/thoughts/** thoughtId/reactions <POST>
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// api/thoughts/** thoughtId/reactionId <DELETE>
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 // Module export router
