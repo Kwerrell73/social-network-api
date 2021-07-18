9a91ce59efd200c81590775ee12aa2406570cf49
Kellie Werrell <kelliewerrell@gmail.com>
2021-07-18 16:15:36 -0700

    added get al users and get user by id to user-controllers

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,25 +1,34 @@
-const { Pizza } = require('../models');
+const { Users } = require('../models');
 
-const pizzaController = {
-  //get all pizzas
-  getAllPizza(req, res) {
-    Pizza.find({})
+const usersController = {
+  // Create a new User
+  createUsers({ body }, res) {
+    Users.create(body)
+      .then(dbUsersData => res.json(dbUsersData))
+      .catch(err => res.status(400).json(err));
+  },
+  //get all users
+  getAllUsers(req, res) {
+    Users.find({})
       .populate({
-        path: 'comments',
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
         select: '-__v'
       })
       .select('-__v')
-      .sort({ _id: -1 })
-      .then(dbPizzaData => res.json(dbPizzaData))
+      .then(dbUsersData => res.json(dbUsersData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
   },
 
-  // get one pizza by id
-  getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
+  // get one user by id
+  getUsersById({ params }, res) {
+    Users.findOne({ _id: params.id })
       .populate({
         path: 'comments',
         select: '-__v'
@@ -45,18 +54,18 @@ const pizzaController = {
       .catch(err => res.status(400).json(err));
   },
 
- // update pizza by id
-updatePizza({ params, body }, res) {
-  Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-    .then(dbPizzaData => {
-      if (!dbPizzaData) {
-        res.status(404).json({ message: 'No pizza found with this id!' });
-        return;
-      }
-      res.json(dbPizzaData);
-    })
-    .catch(err => res.status(400).json(err));
-},
+  // update pizza by id
+  updatePizza({ params, body }, res) {
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
 
   // delete pizza
   deletePizza({ params }, res) {
