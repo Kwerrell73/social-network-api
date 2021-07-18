928b820b020b9fab1e67b421ceea2b80decba711
Kellie Werrell <kelliewerrell@gmail.com>
2021-07-18 15:26:11 -0700

    cleaned up index.js in routes including notation * quick correction made

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,11 @@ const router = require('express').Router();
 
 // API routes import
 const apiRoutes = require('./api');
-//const htmlRoutes = require('./html/html-routes');
-
 
 // add prefix of `/api` to all of the api routes imported from the `api` directory
 router.use('/api', apiRoutes);
 
-
-
+// send error
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
 });
